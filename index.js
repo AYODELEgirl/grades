869a146752a2e0d9685ea7204368608f8bf108ef
AYODELEgirl <timimarvel@gmail.com>
2024-01-10 15:28:17 -0800

    new commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var a = 3
+var b = 6
+
+var c = a
+var a = b
+var b = c
+
+alert('welcome')
+let userName = prompt('what i syour name') 
+alert(userName)
+
+let age = 77
+console.log(`welcome ${userName} you were born in  ${new Date() . getFullYear() -age} `)
+
+prompt('write your tweet')
+ let tweet = prompt('write your tweet')
+let tweetCount = tweet.length 
+alert(`you have written ${tweetCount} you have ${15-tweetCount} characters remaining`)
+ 
