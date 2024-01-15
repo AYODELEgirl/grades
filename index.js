6007200d188ccbf5246140bc1b73d1962b12182c
AYODELEgirl <timimarvel@gmail.com>
2024-01-15 01:32:54 -0800

    new commit

--- index.js
+++ index.js
@@ -12,8 +12,12 @@ alert(userName)
 let age = 77
 console.log(`welcome ${userName} you were born in  ${new Date() . getFullYear() -age} `)
 
-prompt('write your tweet')
- let tweet = prompt('write your tweet')
-let tweetCount = tweet.length 
-alert(`you have written ${tweetCount} you have ${15-tweetCount} characters remaining`)
- 
+// prompt('write your tweet')
+//  let tweet = prompt('write your tweet')
+// let tweetCount = tweet.length 
+// alert(`you have written ${tweetCount} you have ${15-tweetCount} characters remaining`)
+
+let tweet = prompt('write your twwet')
+alert('tweet')
+let tweetCount = tweet.length
+alert('you have written ${tweetCount} , you have ${15-twetCount characters left} ')
